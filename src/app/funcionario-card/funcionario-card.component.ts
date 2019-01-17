04f9edc04b40b0db87e8f2998c80d5b7f6d5d0c1
David Borelli <david_borelli@live.com>
2019-01-17 01:24:52 -0200

    9.17 - Estilos CSS dinâmico com ngStyle

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -5,13 +5,23 @@ import {Component, Input} from '@angular/core';
   templateUrl: './funcionario-card.component.html',
   // styleUrls: ['./funcionario-card.component.css']
   styles: [`
-    .card-block {
+    .card-body {
       text-transform: uppercase;
       color: blue;
+      text-align: center;
     }
   `]
 })
 export class FuncionarioCardComponent {
 
   @Input() funcionario: any;
+
+  // Método retorna um objeto
+  getEstilosCartao() {
+    return {
+      'border-width.px': this.funcionario.id / 10,
+      backgroundColor: this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen'
+    };
+  }
+
 }
